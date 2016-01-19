4e5b0754fbe426e4c0da70cb043752628736d504
AdamsDevelopment <adams.development@hotmail.com>
2016-01-19 11:30:30 +0100

    Made customPokemon and layout

---
+++ Pokédex/Pokemon.swift
@@ -0,0 +1,28 @@
+//
+//  Pokemon.swift
+//  Pokédex
+//
+//  Created by Stefan Adams on 19/01/16.
+//  Copyright © 2016 Adams Development. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+        
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
---
+++ Pokédex/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  Pokédex
+//
+//  Created by Stefan Adams on 19/01/16.
+//  Copyright © 2016 Adams Development. All rights reserved.
+//
+
+import Cocoa
+
+class PokeCell: UICollectionViewCell {
+
+}

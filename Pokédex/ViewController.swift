d0e1f2de2e2482792684d34c80ecd652f71ac8eb
AdamsDevelopment <adams.development@hotmail.com>
2016-01-19 10:53:19 +0100

    Initial Commit

---
+++ Pokédex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokédex
+//
+//  Created by Stefan Adams on 19/01/16.
+//  Copyright © 2016 Adams Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
